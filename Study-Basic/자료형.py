16c9ef5c0f810fbb8685bc268fc715ee3009da6c
enjoy89 <62649762+enjoy89@users.noreply.github.com>
2023-10-03 09:14:46 +0000

    Update 자료형.py

--- Study-Basic/자료형.py
+++ Study-Basic/자료형.py
@@ -28,6 +28,10 @@ print(a % b)
 # 몫
 print(a // b)
 
+# 몫과 나머지
+div, mod = divmod(a, b)
+print(div, mod)
+
 # 거듭 제곱
 print(a ** b)
 
@@ -38,31 +42,33 @@ print(a ** 0.5)
 a = [1, 2, 3, 4, 5]
 print(a)
 
-print(a[2])  # 세 번재 원소에 접근
-print(a[-1])  # 맨 뒤 (다섯 번째 원소에 접근)
+print(a[2])  # 세 번째 원소에 접근
+print(a[-1])  # 뒤에서 첫 번째 원소에 접근
 
 # 리스트 슬라이싱
 # 시작 인덱스 : 끝 인덱스 + 1
 print(a[0:3])  # 첫 번째 원소부터 두 번째 원소까지 출력
 
+# 리스트 초기화
 a = [0] * 10  # 0으로 초기화 된 10칸 짜리 리스트 생성
 print(a)
 
-# 리스트 컴프리헨션
+# 리스트 컴프리헨션(초기화)
 list = [i for i in range(10)]  # 0부터 9까지의 수를 포함하는 리스트
 print(list)
 
 list = [i for i in range(1, 11)]  # 1 ~ 10까지 포함
 print(list)
 
-# 일반적인 코드
+# 0 ~ 20까지의 수 중에서 홀수만 포함하는 리스트 생성 (컴프리헨션)
+list = [i for i in range(20) if i % 2 == 1]  # 0~19까지의 수 중에서 홀수만 포함하는 리스트
+print(list)
+
+# 일반적인 방법
 list = []
 for i in range(20):
     if i % 2 == 1:
-        list.append(i) # append(): 리스트에 원소를 하나 삽입할 때 사용
-print(list)
-
-list = [i for i in range(20) if i % 2 == 1]  # 0~19까지의 수 중에서 홀수만 포함하는 리스트
+        list.append(i)  # append(): 리스트에 원소를 하나 삽입할 때 사용
 print(list)
 
 list = [i * i for i in range(1, 10)]  # 1~9까지의 수들의 제곱값을 포함하는 리스트
@@ -72,9 +78,8 @@ print(list)
 # 4 X 3
 n = 4
 m = 3
-list = [[0] * m for _ in range(n)]
+list = [[0] * m for _ in range(n)]  # 언더바_: 반복을 수행하되 반복을 위한 변수의 값을 무시할 때 사용 -> 단순 반복 작업
 
-# 언더바_: 반복을 수행하되 반복을 위한 변수의 값을 무시할 때 사용 -> 단순 반복 작업
 # 1부터 9까지의 자연수 더하기
 sum = 0
 for i in range(1, 10):
@@ -116,28 +121,37 @@ print("값이 3인 데이터 개수: ", a.count(3))
 a.remove(1)
 print("값이 1인 데이터 삭제: ", a)
 
+# 특정 값을 가지는 원소 모두 삭제
+b = [1, 2, 2, 3, 3, 4, 5]
+remove_set = {2, 3}
+result = [i for i in b if i not in remove_set]
+print(result)
+
 # 4. 문자열 자료형
 print('\"Hello\"')
 
 # 문자열 연산
 a = "ABCDEF"
-print(a[2:4])
+print(a[2:4])  # 인덱스 2부터 3까지
+str1 = "Hello"
+str2 = "World"
+print(str1 + " " + str2)
 
-# 튜플 자료형 - 서로 다른 성질의 데이터를 묶어서 관리해야 할 때 사용
+# 5. 튜플 자료형 - 서로 다른 성질의 데이터를 묶어서 관리해야 할 때 사용
 # 리스트보다 메모리를 효율적으로 사용해야 할 때
 # Hashing의 키 값으로 사용해야할 때
 a = (1, 2, 3, 4, 5)
 
-# 5. 사전 자료형
+# 6. 사전 자료형
 data = dict()
 data['사과'] = 'Apple'
 data['바나나'] = 'Banana'
 data['코코넛'] = 'Coconut'
 
 data = {
-    '사과' : 'Apple',
-    '바나나' : 'Banana',
-    '코코넛' : 'Coconut'
+    '사과': 'Apple',
+    '바나나': 'Banana',
+    '코코넛': 'Coconut'
 }
 
 # 키 데이터만 담은 리스트
@@ -152,7 +166,6 @@ print(data['사과'])
 # 각 키에 따른 값을 하나씩 출력
 for key in key_list:
     print(data[key])
-
 
 # 6. 집합 자료형
 # 순서가 없고, 중복을 허용하지 않음
