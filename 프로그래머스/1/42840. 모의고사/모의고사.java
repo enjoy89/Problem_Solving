fde5bdd2b72508effd9b36bca9ba317c786832ca
enjoy89 <62649762+enjoy89@users.noreply.github.com>
2023-10-19 14:11:40 +0900

    [level 1] Title: 모의고사, Time: 1.62 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        return answer;
+
+        int[] person_1 = {1, 2, 3, 4, 5};                   
+        int[] person_2 = {2, 1, 2, 3, 2, 4, 2, 5};     
+        int[] person_3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};    
+
+        int[] corrects = new int[3];  
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == person_1[i % person_1.length]) {
+                corrects[0]++;
+            }
+            if (answers[i] == person_2[i % person_2.length]) {
+                corrects[1]++;
+            }
+            if (answers[i] == person_3[i % person_3.length]) {
+                corrects[2]++;
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        int max = Arrays.stream(corrects).max().getAsInt();
+
+        for (int i = 0; i < corrects.length; i++) {
+            if (corrects[i] == max) {
+                list.add(i + 1);
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
