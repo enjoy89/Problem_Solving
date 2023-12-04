749793824135f86e37d9dc1c3c511202699007b2
전두이 <62649762+enjoy89@users.noreply.github.com>
2023-12-05 03:10:49 +0900

    [Gold V] Title: ABCDE, Time: 296 ms, Memory: 20700 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean visited[];
+    static boolean check;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        A = new ArrayList[N];
+        visited = new boolean[N];
+        check = false;
+
+        for (int i = 0; i < N; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            A[a].add(b);
+            A[b].add(a);
+        }
+
+        for (int i = 0; i < N; i++) {
+            DFS(i, 1);
+
+            if(check) {
+                break;
+            }
+        }
+
+        if (check) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        br.close();
+    }
+
+    private static void DFS(int v, int depth) {
+        if (depth == 5) {
+            check = true;
+            return;
+        }
+        if (visited[v]) {
+            return;
+        }
+        visited[v] = true;
+
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                DFS(i, depth + 1);
+            }
+        }
+        visited[v] = false;
+    }
+}
