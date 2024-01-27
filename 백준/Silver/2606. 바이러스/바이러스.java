9dd09c59927747c160cc497a80a7d7951a5a310e
전두이 <62649762+enjoy89@users.noreply.github.com>
2024-01-27 16:47:24 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,50 +5,42 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static ArrayList<Integer>[] A;
+    static ArrayList<Integer>[] list;
     static boolean visited[];
-    static int count;
-
+    static int count = 0;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
-
-        A = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-
-        for (int i = 1; i < n + 1; i++) {
-            A[i] = new ArrayList<>();
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        
+        list = new ArrayList[N+1];
+        visited = new boolean[N+1];
+        
+        for(int i=0; i<list.length; i++) {
+            list[i] = new ArrayList<>();
         }
-
-        StringTokenizer st;
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+        
+        for(int i=1; i<=M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-
-            A[start].add(end);
-            A[end].add(start);
+            
+            list[start].add(end);
+            list[end].add(start);
         }
-
-        count = 0;
-        DFS(1);
-        System.out.println(count);
+        dfs(1);
+        System.out.println(count-1);
+        
     }
-
-    private static void DFS(int v) {
-        if (visited[v]) {
-            return;
-        }
-
-        visited[v] = true;
-
-        for (int i : A[v]) {
-            if (!visited[i]) {
-                DFS(i);
-                count++;
+    
+    public static void dfs(int node) {
+        visited[node] = true;
+        count++;
+        
+        for(int i : list[node]) {
+            if(!visited[i]) {
+                dfs(i);
             }
         }
     }
