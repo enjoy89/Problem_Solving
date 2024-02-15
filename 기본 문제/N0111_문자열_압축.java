cf4b551c961511ae0e5c3b9c7c6920871cf4acdb
전두이 <62649762+enjoy89@users.noreply.github.com>
2024-02-16 00:54:35 +0900

    Create N0111_문자열_압축.java

---
+++ 기본 문제/N0111_문자열_압축.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class N0111_문자열_압축 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+
+        int index = 0;
+        while(index < str.length()) {
+            char c = str.charAt(index);
+            int count = 0;
+
+            for(int i=index; i<str.length(); i++) {
+                if(str.charAt(i) == c) {
+                    count++;
+                } else {
+                    break;
+                }
+                index++;
+            }
+            sb.append(c).append(count);
+        }
+
+        String result = sb.toString().replaceAll("1", "");
+        System.out.println(result);
+    }
+}
