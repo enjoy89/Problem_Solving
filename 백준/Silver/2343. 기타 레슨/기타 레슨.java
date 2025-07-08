fe272b6dd1c6aef9779e350cef9be2a1578ddefa
jeondui <62649762+enjoy89@users.noreply.github.com>
2025-07-08 21:44:18 +0900

    [Silver I] Title: 기타 레슨, Time: 304 ms, Memory: 24180 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // 강의의 수
+        int M = Integer.parseInt(st.nextToken()); // 블루레이 개수
         st = new StringTokenizer(br.readLine());
         int[] arr = new int[N];
         int max = 0;
@@ -15,31 +15,33 @@ public class Main {
             if(arr[i] > max) max = arr[i];
         }
 
-        long left = max; // 강의 길이의 최대값
-        long right = Arrays.stream(arr).sum(); // 강의 길이의 총합
+        long left = max; // 강의 길이의 최댓값
+        long right = Arrays.stream(arr).sum(); // 모든 강의 길이의 합
         long answer = 0;
         while(left <= right) {
             long mid = (left + right) / 2;
+            long count = 1; // 개수는 1부터 시작
             long sum = 0;
-            int cnt = 1;
             for(int i=0; i<N; i++) {
                 long tmp = sum + arr[i];
                 if(tmp > mid) {
-                    cnt++;
-                    sum = arr[i]; // 새로운 길이로 시작
+                    count++;
+                    sum = arr[i];
                 } else {
                     sum += arr[i];
                 }
             }
-
-            if(cnt <= M) {
+            // 블루레이 개수가 M보다 작거나 같으면 조건 만족
+            // 길이를 줄여서 재탐색
+            if(count <= M) {
                 answer = mid;
-                right = mid - 1;
+                right =  mid - 1;
             } else {
+                // 조건 불만족
+                // 길이를 늘려서 재탐색
                 left = mid + 1;
             }
         }
         System.out.println(answer);
-
     }
 }
