1cd2e6b12f4895bc6628409f2fa518439bbc540d
Dui Jeon <jeondui@gmail.com>
2025-08-28 14:41:16 +0900

    [level 1] Title: 예산, Time: 1.14 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        Arrays.sort(d);
-        
-        int left = 0;
-        int right = d.length; // 모든 부서의 수
-        while(left <= right) {
-            int mid = (left+right) / 2;
-            int sum = 0;
-            for(int i=0; i<mid; i++) {
-                sum += d[i];
-            }
-            
-            if(sum <= budget) {
-                answer = mid;
-                left = mid+1;
-            } else {
-                right = mid-1;
-            }
-        }
-        
         return answer;
     }
 }
