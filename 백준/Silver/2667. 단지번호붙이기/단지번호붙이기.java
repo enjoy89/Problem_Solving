99ffc866c26979e4ba1090bd0f6af2c0ac1cdeae
Dui Jeon <jeondui@gmail.com>
2025-08-30 19:57:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,69 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
-
     static int[][] map;
-    static boolean visited[][];
-    static int dx[] = {-1, 1, 0, 0};    
-    static int dy[] = {0, 0, -1, 1};    
-    static int N, count;
+    static boolean[][] visited;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+    static int N;
+    static int houseCount;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> result = new ArrayList<>();
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
         visited = new boolean[N][N];
-        count = 0;
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Character.getNumericValue(str.charAt(j));
+        for(int i=0; i<N; i++) {
+            String[] nums = br.readLine().split("");
+            for(int j=0; j<nums.length; j++) {
+                map[i][j] = Integer.parseInt(nums[j]);
             }
+
         }
 
-        int num = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                count = 0;
-                if (map[i][j] == 1 & !visited[i][j]) {
-                    DFS(i, j);
-                    result.add(count);
-                    num++;
+        int count = 0;
+        List<Integer> result = new ArrayList<>();
+        for(int y=0; y<N; y++) {
+            for(int x=0; x<N; x++) {
+                if(map[y][x] == 1 && !visited[y][x]) {
+                    houseCount = 1;
+                    dfs(y, x);
+                    result.add(houseCount);
+                    count++;
                 }
             }
         }
-
-        System.out.println(num);
         Collections.sort(result);
-        for (int count : result) {
-            System.out.println(count);
+        System.out.println(count);
+        for(int num : result) {
+            System.out.println(num);
         }
     }
 
-    private static void DFS(int x, int y) {
-        visited[x][y] = true;
-        count += 1;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
+    public static void dfs(int y, int x) {
+        visited[y][x] = true;
+        for(int i=0; i<4; i++) {
             int ny = y + dy[i];
-
-            if (checkRange(nx, ny) && !visited[nx][ny] && map[nx][ny] == 1) {
-                DFS(nx, ny);
+            int nx = x + dx[i];
+            if(ny >= 0 && ny < N && nx >= 0 && nx < N) {
+                if(map[ny][nx] == 1 && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    houseCount++;
+                    dfs(ny, nx);
+                }
             }
         }
-    }
-
-    private static boolean checkRange(int nx, int ny) {
-        return nx >= 0 && nx < N && ny >= 0 && ny < N;
     }
 }
