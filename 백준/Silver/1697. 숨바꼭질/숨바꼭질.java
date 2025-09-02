b078ebece0d7389c3d8957c764414bcc0a21041f
Dui Jeon <jeondui@gmail.com>
2025-09-02 10:34:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 23916 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,36 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, K;
-
+    static int K;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); // 시작위치
-        K = Integer.parseInt(st.nextToken()); // 도착 위치
+        int N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
         bfs(N);
-
     }
 
     public static void bfs(int start) {
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[100001];
-        int[] dist = new int[1000001];
+        int[] result = new int[100001]; // 걸리는 시간을 누적해서 기록
 
         visited[start] = true;
         queue.offer(start);
 
         while(!queue.isEmpty()) {
             int cur = queue.poll();
-            if(cur == K) {
-                System.out.println(dist[cur]);
-            }
 
-            for(int next: new int[]{cur-1, cur+1, cur*2}) {
-                if(next >=0 && next <= 100000 && !visited[next]) {
-                    dist[next] = dist[cur] + 1;
+            if(cur == K) {
+                System.out.println(result[cur]);
+                
+            }
+            for(int next : new int[]{cur-1, cur+1, cur*2}) {
+                if(next>=0 && next <= 100000 && !visited[next]) {
+                    result[next] = result[cur] + 1;
                     visited[next] = true;
                     queue.offer(next);
                 }
