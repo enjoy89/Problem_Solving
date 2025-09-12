b19e7cb6b11a49a187e6f9ba46c7f4fbad3d1581
Dui Jeon <jeondui@gmail.com>
2025-09-12 15:07:34 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,62 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    static class Document {
+        int index;
+        int priority;
+
+        public Document(int index, int priority) {
+            this.index = index;
+            this.priority = priority;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        LinkedList<int[]> queue;
+        int T = Integer.parseInt(br.readLine());
 
-        int testCase = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < testCase; i++) {
+        for(int t=0; t<T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken()); // 문서의 개수
+            int M = Integer.parseInt(st.nextToken()); // 알고싶은 문서의 인덱스
 
-            int N = Integer.parseInt(st.nextToken());   // 문서의 개수
-            int M = Integer.parseInt(st.nextToken());   // 문서의 순서
+            Queue<Document> q = new LinkedList<>();
+            PriorityQueue<Document> pq = new PriorityQueue<>(
+                    (d1, d2) -> Integer.compare(d2.priority, d1.priority)
+            );
 
-            st = new StringTokenizer(br.readLine());    // 문서의 중요도
-            queue = new LinkedList<>();
-            
-            for (int j = 0; j < N; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                queue.offer(new int[]{j, num});
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) {
+                int priority = Integer.parseInt(st.nextToken());
+                Document doc = new Document(i, priority);
+                q.offer(doc);
+                pq.offer(doc);
             }
 
             int count = 0;
+            while(!q.isEmpty()) {
+                Document cur = q.poll();
+                Document front = pq.peek();
 
-            while (!queue.isEmpty()) {
-                int[] first = queue.poll();
-                boolean isMax = true;
+                if(cur.priority == front.priority) {
+                    count++;
+                    pq.poll();
 
-                // 현재 문서보다 중요도가 높은 문서가 있다면 맨 뒤로 넘김
-                for (int j = 0; j < queue.size(); j++) {
-                    if (first[1] < queue.get(j)[1]) {
-                        queue.offer(first);
-
-                        for (int z = 0; z < j; z++) {
-                            queue.offer(queue.poll());
-                        }
-                        isMax = false;
+                    if(cur.index == M) {
+                        System.out.println(count);
                         break;
                     }
-                }
-
-                if (!isMax) {
-                    continue;
-                }
-
-                count++;
-                if (first[0] == M) {
-                    sb.append(count).append("\n");
-                    break;
+                } else {
+                    q.offer(cur);
                 }
             }
         }
-        System.out.println(sb);
     }
 }
