f165b6ff49aec18524ee9f1c23888ec5bf3af7cf
Dui Jeon <jeondui@gmail.com>
2025-09-17 21:40:22 +0900

    [Bronze III] Title: 전자레인지, Time: 136 ms, Memory: 16288 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.java
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -5,24 +5,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        // 300으로 나누어 떨어질 때까지 60, 10을 뺀다
         int A = 0; // 300
         int B = 0; // 60
         int C = 0; // 10
-        while(T >= 0) {
-            if(T % 300 == 0) {
-                A += T / 300;
-                System.out.println(A + " " + B + " " + C);
-                return;
-            }
-            if(T >= 60) {
-                T -= 60;
-                B++;
-            } else {
-                T -= 10;
-                C++;
-            }
+
+        if (T % 10 != 0) {
+            System.out.println(-1);
+            return;
         }
-        System.out.println(-1);
+
+        A += T / 300;
+        T %= 300;
+
+        B += T / 60;
+        T %= 60;
+
+        C += T / 10;
+        T %= 10;
+
+        System.out.println(A + " " + B + " " + C);
     }
 }
