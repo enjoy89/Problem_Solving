68bdca45a621dca4d8a78aa145ca2ecbe4c94811
Dui Jeon <jeondui@gmail.com>
2025-09-26 00:48:15 +0900

    [Bronze II] Title: 피보나치 수, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,29 +1,17 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int[] fibo;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        fibo = new int[n+1];
-        recursive(n);
-        System.out.println(fibo[n]);
-    }
+        int[] dp = new int[n+1];
 
-    // 메모제이션 재귀 방법
-    public static int recursive(int n) {
-        if(fibo[n] != 0) {
-            return fibo[n];
-        } else if(n==1) {
-            return fibo[1] = 1;
-        } else if(n==2) {
-            return fibo[2] = 1;
-        } else if(n==0) {
-            return fibo[0] = 0;
-        } else {
-            return fibo[n] = recursive(n-2) + recursive(n-1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
         }
+
+        System.out.println(dp[n]);
     }
 }
