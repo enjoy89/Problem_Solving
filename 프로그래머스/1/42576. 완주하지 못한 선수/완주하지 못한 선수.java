c332297b156e9bb0b58236152d4161be53bf46ae
Dui Jeon <jeondui@gmail.com>
2025-10-23 21:55:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 103.40 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,21 +2,32 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        for(String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1); // 이름이 등장한 빈도수 저장
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        for(int i=0; i<participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1); // 이름이 등장한 빈도수도 함께 저장
         }
-
-        for (String name : completion) {
-            map.put(name, map.get(name) - 1);
-        }
-
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
-                return key;
+        
+        String answer = "";
+        
+        for(int i=0; i<completion.length; i++) {
+            String cur = completion[i];
+            if(map.containsKey(cur)) {
+                if(map.get(cur) == 1) {
+                    map.remove(cur);
+                } else {
+                    map.put(cur, map.get(cur) - 1);
+                }
             }
         }
-
-        return "";
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                answer = key;
+            }
+        }
+        
+        return answer;
+        
     }
 }
