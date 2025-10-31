6839d37d3e8ed750ef9ba1d014c3d2d59f688d48
Dui Jeon <jeondui@gmail.com>
2025-10-31 17:14:37 +0900

    [Gold IV] Title: 최단경로, Time: 856 ms, Memory: 111704 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,93 +1,79 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class Node implements Comparable<Node> {
-        int end, weight;
 
-        public Node(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
+    static class Node implements Comparable<Node> {
+        int vertex; // 도착 정점
+        int cost;   // 거리
+
+        public Node(int vertex, int cost) {
+            this.vertex = vertex;
+            this.cost = cost;
         }
 
         @Override
-        public int compareTo(Node n) {
-            return this.weight - n.weight;
+        public int compareTo(Node o) {
+            return Integer.compare(this.cost, o.cost); // 오름차순 정렬
         }
     }
 
     static final int INF = Integer.MAX_VALUE;
-    static int V, E, start;
-    static ArrayList<Node>[] graph;
-    static int[] distance;
-    static boolean[] visited;
+    static int V, E, K;
+    static List<Node>[] graph;
+    static int[] dist;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        start = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken()); // 정점의 개수
+        E = Integer.parseInt(st.nextToken()); // 간선의 개수
+        K = Integer.parseInt(br.readLine());  // 시작 정점의 번호
 
         graph = new ArrayList[V+1];
-        distance = new int[V+1];
-        visited = new boolean[V+1];
-
         for(int i=1; i<=V; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        // 최단 거리 배열 초기화
-        Arrays.fill(distance, INF);
-
         for(int i=0; i<E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
             graph[u].add(new Node(v, w));
         }
 
-        dijkstra(start);
-
+        dist = new int[V+1];
+        Arrays.fill(dist, INF);
+        dijkstra(K);
         for(int i=1; i<=V; i++) {
-            if(distance[i] == INF) {
+            if(dist[i] == INF) {
                 System.out.println("INF");
             } else {
-                System.out.println(distance[i]);
+                System.out.println(dist[i]);
             }
         }
-
     }
 
     public static void dijkstra(int start) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        distance[start] = 0;
+        pq.add(new Node(start, 0));
+        dist[start] = 0;
 
         while(!pq.isEmpty()) {
-            Node curNode = pq.poll();
+            Node cur = pq.poll();
+            if(cur.cost > dist[cur.vertex]) {
+                continue;
+            }
 
-            visited[curNode.end] = true;
-
-            for(int i=0; i<graph[curNode.end].size(); i++) {
-                Node node = graph[curNode.end].get(i);
-
-                if(!visited[node.end] && distance[node.end] > distance[curNode.end] + node.weight) {
-                    distance[node.end] = distance[curNode.end] + node.weight;
-                    pq.offer(new Node(node.end, distance[node.end]));
+            for(Node next: graph[cur.vertex]) {
+                int newCost = cur.cost + next.cost;
+                if(newCost < dist[next.vertex]) {
+                    dist[next.vertex] = newCost;
+                    pq.add(new Node(next.vertex, newCost));
                 }
-
             }
         }
+
     }
 }
-
-
