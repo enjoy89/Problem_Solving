5940728061c518c8b265e92a8d59d05f43d31afc
Dui Jeon <jeondui@gmail.com>
2025-11-13 19:11:48 +0900

    [Silver I] Title: 오르막 수, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -4,31 +4,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int MOD = 10007;
 
         int[][] dp = new int[N+1][10]; // 길이가 N이고 끝자리가 0~9인 오르막 수
+
+        // 초기값
         for(int i=0; i<10; i++) {
-            dp[1][i] = 1;
+            dp[1][i] = 1; // 0~9, 길이가 1인 오르막 수는 각각 1개이다.
         }
 
-        for(int n=2; n<=N; n++) {
-            for(int i=0; i<10; i++) {
-                if(i == 0) {
+        for (int n = 2; n <= N; n++) {
+            for (int i = 0; i < 10; i++) {
+                // 끝자리가 0인 오르막 수는 1개로 고정
+                if (i == 0) {
                     dp[n][i] = 1;
                 }
                 else {
-                    int sum = 0;
                     for (int j = 0; j <= i; j++) {
-                        sum += dp[n - 1][j];
+                        dp[n][i] += dp[n - 1][j];
                     }
-                    dp[n][i] = sum % 10007;
+                    dp[n][i] = dp[n][i] % MOD;
                 }
-
             }
         }
 
         int answer = 0;
         for(int i=0; i<10; i++) {
-            answer = (answer + dp[N][i]) % 10007;
+            answer = (answer + dp[N][i]) % MOD;
         }
         System.out.println(answer);
     }
