f15b4558566781ffc110cf83b33a05ce1db5528c
Dui Jeon <jeondui@gmail.com>
2025-11-13 17:45:48 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -4,20 +4,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] dp = new int[N+1][10];
 
-        // 초기값
+        int[][] dp = new int[N+1][10]; // 길이가 N이고 끝자리가 0~9인 오르막 수
         for(int i=0; i<10; i++) {
             dp[1][i] = 1;
         }
 
-        for(int i=2; i<N+1; i++) {
-            int sum = 0;
-            for(int j=0; j<10; j++) {
-                sum += dp[i-1][j];
-                dp[i][j] = sum % 10007;
+        for(int n=2; n<=N; n++) {
+            for(int i=0; i<10; i++) {
+                if(i == 0) {
+                    dp[n][i] = 1;
+                }
+                else {
+                    int sum = 0;
+                    for (int j = 0; j <= i; j++) {
+                        sum += dp[n - 1][j];
+                    }
+                    dp[n][i] = sum % 10007;
+                }
+
             }
         }
+
         int answer = 0;
         for(int i=0; i<10; i++) {
             answer = (answer + dp[N][i]) % 10007;
