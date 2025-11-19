8a8b4bb4d542b74347dbdcc2d0c610438d359b39
Dui Jeon <jeondui@gmail.com>
2025-11-19 16:26:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 244 ms, Memory: 30452 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,39 +1,37 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n+1][n+1];
+        int[][] arr = new int[n][n];
 
-        for(int i=1; i<=n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j=1; j<i+1; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        for(int i=0; i<n; i++) {
+            String[] line = br.readLine().split(" ");
+            for(int j=0; j< line.length; j++) {
+                arr[i][j] = Integer.parseInt(line[j]);
             }
         }
 
-        int[][] dp = new int[n+1][n+1];
-        dp[1][1] = arr[1][1];
+        int[][] dp = new int[n][n];
+        dp[0][0] = arr[0][0];
 
-        for(int i=2; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                if(j==1) {
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(j == 0) {
                     dp[i][j] = dp[i-1][j] + arr[i][j];
-                } else if(j==i) {
-                    dp[i][j] = dp[i-1][j-1] + arr[i][j];
                 } else {
                     dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + arr[i][j];
                 }
             }
         }
-
         int answer = 0;
-        for(int i=1; i<=n; i++) {
-            answer = Math.max(answer, dp[n][i]);
+        for(int num: dp[n-1]) {
+            answer = Math.max(answer, num);
         }
-        System.out.println(answer);
 
+        System.out.println(answer);
     }
+
+
 }
