26eef439a267419221a43364695132f3d48babb5
Dui Jeon <jeondui@gmail.com>
2025-12-08 11:51:22 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 120304 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,72 +2,83 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] map;
-    static int N, M;
-    static int[] dy = {-1, 1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
+	static int[][] map;
+	static int N, M;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken()); // 가로
-        N = Integer.parseInt(st.nextToken()); // 세로
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[M][N];
 
-        map = new int[N][M];
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        bfs();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-        boolean check = true;
-        int max = 0;
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(map[i][j] == 0) {
-                    check = false; // 0이 1개라도 있으면 안 익은 토마토가 있으므로 결과 -1
-                }
+		// 처음부터 모든 토마토가 익어있으면 0 출력 후 종료
+		boolean check = false;
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == 0) {
+					check = true;
+				}
+			}
+		}
+		if (!check) {
+			System.out.println(0);
+			return;
+		}
 
-                if(map[i][j] > max) {
-                    max = map[i][j];
-                }
-            }
-        }
+		bfs();
 
-        if(check) {
-            System.out.println(max -1);
-        } else {
-            System.out.println(-1);
-        }
-    }
+		int answer = -1;
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				// 탐색 후 안 익은 토마토가 남아 있으면 -1 출력 후 종료
+				if (map[i][j] == 0) {
+					System.out.println(-1);
+					return;
+				} else {
+					answer = Math.max(answer, map[i][j]);
+				}
+			}
+		}
+		System.out.println(answer - 1);
 
-    public static void bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        for(int y=0; y<N; y++) {
-            for(int x=0; x<M; x++) {
-                if(map[y][x] == 1) {
-                    queue.offer(new int[]{y, x}); // 1(익은 토마토)은 모두 큐에 넣고 시작
-                }
-            }
-        }
+	}
 
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int cy = cur[0];
-            int cx = cur[1];
+	public static void bfs() {
+		// 익은 토마토를 모두 큐에 넣고 시작
+		Queue<int[]> queue = new LinkedList<>();
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				if (map[i][j] == 1) {
+					queue.offer(new int[] {i, j});
+				}
+			}
+		}
 
-            for(int i=0; i<4; i++) {
-                int ny = dy[i] + cy;
-                int nx = dx[i] + cx;
-                if(ny >= 0 && ny < N && nx >= 0 && nx < M) {
-                    if(map[ny][nx] == 0) {
-                        map[ny][nx] = map[cy][cx] + 1;
-                        queue.offer(new int[]{ny, nx});
-                    }
-                }
-            }
-        }
-    }
+		while (!queue.isEmpty()) {
+			int[] cur = queue.poll();
+			int cy = cur[0];
+			int cx = cur[1];
+
+			for (int i = 0; i < 4; i++) {
+				int ny = cy + dy[i];
+				int nx = cx + dx[i];
+				if (ny >= 0 && ny < M && nx >= 0 && nx < N) {
+					if (map[ny][nx] == 0) {
+						map[ny][nx] = map[cy][cx] + 1;
+						queue.offer(new int[] {ny, nx});
+					}
+				}
+			}
+		}
+	}
 }
