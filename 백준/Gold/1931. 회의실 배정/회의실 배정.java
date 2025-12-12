20502951ce28028ae9e2ae2f4b9759e5b85b7e9f
Dui Jeon <jeondui@gmail.com>
2025-12-12 11:08:16 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 43752 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -5,30 +5,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] meetings = new int[N][2];
-
+        int[][] time = new int[N][2];
+        
         for(int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            meetings[i][0] = Integer.parseInt(st.nextToken()); // 시작 시간
-            meetings[i][1] = Integer.parseInt(st.nextToken()); // 종료 시간
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(meetings, (a, b) -> {
-            if(a[1] == b[1]) {
-                return a[0] - b[0]; // 종료 시간이 같으면 시작 시간으로 오름차순
-            } else {
-                return a[1] - b[1]; // 종료 시간으로 오름차순
+        
+        Arrays.sort(time, new Comparator<int[]>() {
+           @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
             }
         });
-
+        
         int count = 0;
-        int end = 0; // 직전 회의의 종료 시간
-
+        int prev_end_time = 0;
         for(int i=0; i<N; i++) {
-            if(meetings[i][0] >= end) {
-                end = meetings[i][1];
+            // 이전 회의 종료 시간이 다음 회의 시작 시간 보다 작거나 같으면 갱신
+            int next_start_time = time[i][0];
+            if(prev_end_time <= next_start_time) {
                 count++;
+                prev_end_time = time[i][1];
             }
         }
+        
         System.out.println(count);
     }
 }
