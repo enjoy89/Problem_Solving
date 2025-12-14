a7debf7ec13bafbcb76aa6af95d01dd246a4c991
Dui Jeon <jeondui@gmail.com>
2025-12-14 22:47:02 +0900

    [Silver III] Title: 퇴사, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,37 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] T, P;
-    static int N;
-    static int answer = 0;
+	static int N;
+	static int[] T;
+	static int[] P;
+	static int answer = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        T = new int[N+1]; // 상담 시간
-        P = new int[N+1]; // 금액
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		T = new int[N];
+		P = new int[N];
 
-        for(int i=1; i<=N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
-        }
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
 
-        dfs(1, 0);
-        System.out.println(answer);
-    }
+		dfs(0, 0);
+		System.out.println(answer);
 
-    public static void dfs(int index, int sum) {
-        // 종료조건: N일 이상인 경우
-        if(index >= N+1) {
-            answer = Math.max(answer, sum);
-            return;
-        }
+	}
 
-        // 상담 가능한 경우
-        if(index + T[index] <= N+1) {
-            dfs(index + T[index], sum + P[index]);
-        }
-        dfs(index+1, sum);
-    }
+    public static void dfs(int day, int sum) {
+		if (day >= N) {
+			answer = Math.max(answer, sum);
+			return;
+		}
+
+		if (day + T[day] <= N) {
+			dfs(day + T[day], sum + P[day]);
+		}
+		dfs(day + 1, sum);
+
+	}
 }
