3fb5afb4a1c844d20c292420ba30dcc2ecbb3aeb
Dui Jeon <jeondui@gmail.com>
2025-12-28 00:34:21 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22052 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,38 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[] arr;
-    static boolean[] visited;
+	static int N, M;
+	static boolean[] visited;
+	static int[] selected;
+	static StringBuilder sb;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-        arr = new int[N];
-        visited = new boolean[N+1];
-        dfs(0);
-    }
+		visited = new boolean[N + 1];
+		selected = new int[N + 1];
+		sb = new StringBuilder();
 
-    public static void dfs(int depth) {
-        if(depth == M) {
-            for(int i=0; i<M; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-            return;
-        }
+		dfs(0);
+		System.out.println(sb);
 
-        for(int i=1; i<=N; i++) {
-            if(!visited[i]) {
-                visited[i] = true; // 방문 처리
-                arr[depth] = i; // 수열에 숫자 저장
-                dfs(depth + 1); // 다음 깊이로
-                visited[i] = false; // 방문 처리 초기화 (백트래킹)
-            }
-        }
-    }
+	}
+
+	// n: 선택된 숫자 개수
+	public static void dfs(int n) {
+		if (n == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 1; i < N + 1; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				selected[n] = i;
+				dfs(n + 1);
+				visited[i] = false;
+			}
+		}
+	}
 }
