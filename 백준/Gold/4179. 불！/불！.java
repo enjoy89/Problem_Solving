54ab06d96be5d8a490e5bea1dd5a56bb101b45b4
Dui Jeon <jeondui@gmail.com>
2026-01-08 13:35:17 +0900

    [Gold III] Title: 불!, Time: 1152 ms, Memory: 160628 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -3,119 +3,118 @@ import java.util.*;
 
 public class Main {
 
-	static int[] dy = {-1, 1, 0, 0};
-	static int[] dx = {0, 0, -1, 1};
-	static int R, C;
-	static char[][] map;
-	static boolean[][] visited_fire;
-	static boolean[][] visited_jh;
-	static int[][] fire;
-	static int[][] jh;
-	static boolean possible;
+    static int R, C;
+    static int[] dx = { -1, 1, 0, 0 };
+    static int[] dy = { 0, 0, -1, 1 };
+    static char[][] board;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+    static boolean[][] fire_visited;
+    static int[][] fire;
+    static boolean[][] jh_visited;
+    static int[][] jh;
 
-		map = new char[R][C];
-		visited_fire = new boolean[R][C];
-		visited_jh = new boolean[R][C];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        board = new char[R][C];
 
-		for (int i = 0; i < R; i++) {
-			String[] line = br.readLine().split("");
-			for (int j = 0; j < C; j++) {
-				map[i][j] = line[j].charAt(0);
-			}
-		}
+        fire_visited = new boolean[R][C];
+        fire = new int[R][C];
+        Queue<int[]> fire_queue = new LinkedList<>();
 
-		fire = new int[R][C];
-		jh = new int[R][C];
+        jh_visited = new boolean[R][C];
+        jh = new int[R][C];
+        Queue<int[]> jh_queue = new LinkedList<>();
 
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				fire[i][j] = Integer.MAX_VALUE;
-			}
-		}
+        for (int i = 0; i < R; i++) {
+            Arrays.fill(fire[i], -1);
+            Arrays.fill(jh[i], -1);
+        }
 
-		Queue<int[]> fire_queue = new LinkedList<>();
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] == 'F') {
-					visited_fire[i][j] = true;
-					fire[i][j] = 1;
-					fire_queue.offer(new int[] {i, j});
-				}
-			}
-		}
-		fire(fire_queue);
+        for (int i = 0; i < R; i++) {
+            String[] line = br.readLine().split("");
+            for (int j = 0; j < C; j++) {
+                char c = line[j].charAt(0);
+                if (c == 'J') {
+                    jh[i][j] = 0;
+                    jh_queue.offer(new int[] { i, j });
+                    jh_visited[i][j] = true;
+                } else if (c == 'F') {
+                    fire[i][j] = 0;
+                    fire_queue.offer(new int[] { i, j });
+                    fire_visited[i][j] = true;
+                }
+                board[i][j] = c;
+            }
+        }
 
-		for (int i = 0; i < R; i++) {
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] == 'J') {
-					visited_jh[i][j] = true;
-					jh[i][j] = 1;
-					possible = bfs(i, j);
-				}
-			}
-		}
+        fire_bfs(fire_queue);
+        jh_bfs(jh_queue);
 
-		if (!possible) {
-			System.out.println("IMPOSSIBLE");
-		}
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (i == 0 || i == R - 1 || j == 0 || j == C - 1) {
+                    if (jh[i][j] != -1) {
+                        answer = Math.min(answer, jh[i][j]);
+                    }
+                }
+            }
+        }
 
-	}
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println("IMPOSSIBLE");
+        } else {
+            System.out.println(answer + 1);
+        }
+    }
 
-	public static void fire(Queue<int[]> queue) {
-		while (!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int cy = cur[0];
-			int cx = cur[1];
+    public static void fire_bfs(Queue<int[]> queue) {
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
 
-			for (int i = 0; i < 4; i++) {
-				int ny = cy + dy[i];
-				int nx = cx + dx[i];
-				if (ny >= 0 && ny < R && nx >= 0 && nx < C) {
-					if (!visited_fire[ny][nx] && map[ny][nx] != '#') {
-						visited_fire[ny][nx] = true;
-						fire[ny][nx] = fire[cy][cx] + 1;
-						queue.offer(new int[] {ny, nx});
-					}
-				}
-			}
-		}
-	}
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-	public static boolean bfs(int y, int x) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {y, x});
+                if (isRange(nx, ny) && board[nx][ny] != '#' && !fire_visited[nx][ny]) {
+                    fire_visited[nx][ny] = true;
+                    fire[nx][ny] = fire[cx][cy] + 1;
+                    queue.offer(new int[] { nx, ny });
+                }
+            }
+        }
+    }
 
-		while (!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int cy = cur[0];
-			int cx = cur[1];
+    public static void jh_bfs(Queue<int[]> queue) {
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
 
-			// 탈출 여부 확인
-			if (cy == 0 || cy == R - 1 || cx == 0 || cx == C - 1) {
-				System.out.println(jh[cy][cx]);
-				return true;
-			}
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
 
-			for (int i = 0; i < 4; i++) {
-				int ny = cy + dy[i];
-				int nx = cx + dx[i];
-				if (ny >= 0 && ny < R && nx >= 0 && nx < C) {
-					// 불보다 먼저 도착한 경우
-					if (!visited_jh[ny][nx] && map[ny][nx] == '.' && (jh[cy][cx] + 1 < fire[ny][nx])) {
-						visited_jh[ny][nx] = true;
-						jh[ny][nx] = jh[cy][cx] + 1;
-						queue.offer(new int[] {ny, nx});
-					}
-				}
-			}
-		}
+                if (isRange(nx, ny) && board[nx][ny] != '#' && !jh_visited[nx][ny]) {
+                    // 지훈이보다 불이 먼저 도착하면 방문 불가능
+                    if (!fire_visited[nx][ny] || jh[cx][cy] + 1 < fire[nx][ny]) {
+                        jh_visited[nx][ny] = true;
+                        jh[nx][ny] = jh[cx][cy] + 1;
+                        queue.offer(new int[] { nx, ny });
+                    }
+                }
+            }
+        }
 
-		return false;
-	}
+    }
+
+    public static boolean isRange(int x, int y) {
+        return x >= 0 && x < R && y >= 0 && y < C;
+    }
+
 }
